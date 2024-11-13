c54003f8343bb856484ddc406096f25345e3993d
ifrah-ashraf <ifrahashraf48@gmail.com>
2024-11-13 22:09:02 +0530

    feat: proper synchronization working

--- main.go
+++ main.go
@@ -5,36 +5,46 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"sync"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
+	t1 := time.Now()
 
-	var urls = []string{"https://news.ycombinator.com/"}
-
+	var urls = []string{"https://news.ycombinator.com/", "https://monkeytype.com/", "https://www.amazon.in/"}
+	var wgSend sync.WaitGroup
+	var wgRec sync.WaitGroup
 	linkCH := make(chan string)
 
 	for _, url := range urls {
-
+		wgSend.Add(1)
 		go func(url string) {
-
+			defer wgSend.Done()
 			linkCH <- url
 		}(url)
 	}
 
 	go func() {
-
-		for link := range linkCH {
-			FetchLink(link)
-		}
-
+		wgSend.Wait()
+		close(linkCH)
 	}()
 
-	defer close(linkCH)
+	for link := range linkCH {
+		wgRec.Add(1)
+		go func(link string) {
+			defer wgRec.Done()
+			FetchLink(link)
+		}(link)
+	}
 
-	time.Sleep(2 * time.Second)
+	wgRec.Wait()
+	t2 := time.Now()
+	diff := t2.Sub(t1)
+	fmt.Println("total Time taken is : ", diff)
+
 }
 
 func FetchLink(link string) {
@@ -43,43 +53,38 @@ func FetchLink(link string) {
 
 	var urlArr []string
 
-	go func(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Failed to fetch url: ", url, err)
+		return
+	}
+	defer resp.Body.Close()
 
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Println("Failed to fetch url: ", url, err)
-			return
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Failed to fetch %s: %d %s", url, resp.StatusCode, resp.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		fmt.Println("failed while reading the body in goquery: ", err.Error())
+	}
+
+	doc.Find("a").Each(func(i int, elem *goquery.Selection) {
+
+		//atag := elem.Find("a").First()
+		link, ok := elem.Attr("href")
+		if ok {
+			urlArr = append(urlArr, link)
 		}
-		defer resp.Body.Close()
 
-		if resp.StatusCode != http.StatusOK {
-			log.Fatalf("Failed to fetch %s: %d %s", url, resp.StatusCode, resp.Status)
-		}
+	})
 
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
-		if err != nil {
-			fmt.Println("failed while reading the body in goquery: ", err.Error())
-		}
+	fmt.Println("received link is ", url)
+	fmt.Println()
+	UrlValidator(urlArr)
 
-		doc.Find("a").Each(func(i int, elem *goquery.Selection) {
-
-			//atag := elem.Find("a").First()
-			link, ok := elem.Attr("href")
-			if ok {
-				urlArr = append(urlArr, link)
-			}
-
-		})
-
-		fmt.Println("received link is ", url)
-		fmt.Println()
-		UrlValidator(urlArr)
-		/* for i, url := range urlArr {
-			fmt.Printf("URL %d is %s\n", i, url)
-		} */
-		fmt.Printf("\nURL BREAK\n")
-		fmt.Println()
-	}(url)
+	fmt.Printf("\nURL BREAK\n")
+	fmt.Println()
 
 }
 
@@ -97,3 +102,15 @@ func UrlValidator(urlArr []string) {
 	}
 	fmt.Printf("\nTotal valid URL count is %d\n", count)
 }
+
+/* go func() {
+
+	for link := range linkCH {
+		FetchLink(link)
+	}
+
+}() */
+
+/* for i, url := range urlArr {
+	fmt.Printf("URL %d is %s\n", i, url)
+} */
